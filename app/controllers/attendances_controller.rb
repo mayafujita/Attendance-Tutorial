91ed9408e45ff38924b511407052b2b8cd9e2f6e
mayafujita <maya.shocora324@gmail.com>
2020-03-21 06:08:42 +0000

    11章2 StrongParamenters応用

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
 
@@ -29,6 +29,11 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-    debugger
   end
+  
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])
+    end
 end
